622c86807980cd6adc9f7a5e8f056fa0321c5790
FernandoTM13 <fernando.villegas1@unmsm.edu.pe>
2023-02-26 21:46:04 -0500

    Move methods

--- Original.src/chessboard_entities/ChessMenuBar.java
+++ Original.src/chessboard_entities/ChessMenuBar.java
@@ -85,44 +85,44 @@ public class ChessMenuBar
          * Takes an appropriate action if the restart button is clicked.
          */
         
+        private void restartHandler(){
+            ( (ChessPanel)getParent() ).getGameEngine().reset();
+        }
+        /**
+         * Takes an appropriate action if the exit button is clicked.
+         * Uses Tony Allevato's code for exiting a GUI app without System.exit()
+         * calls.
+         */
+        
+        private void exitHandler() {
+            JOptionPane.showMessageDialog(getParent(), "Thanks for leaving, quitter! >:(");
+            Component possibleFrame = this;
+            while (possibleFrame != null && !(possibleFrame instanceof JFrame)) {
+                possibleFrame = possibleFrame.getParent();
+            }
+            JFrame frame = (JFrame) possibleFrame;
+            frame.setVisible(false);
+            frame.dispose();
+        }
+        
+        /**
+         * Takes an appropriate action if the toggle graveyard button is clicked.
+         */
+        private void toggleGraveyardHandler(){
+            ( (ChessPanel)getParent() ).getGraveyard( 1 ).setVisible(
+                !( (ChessPanel)getParent() ).getGraveyard( 1 ).isVisible() );
+            ( (ChessPanel)getParent() ).getGraveyard( 2 ).setVisible(
+                !( (ChessPanel)getParent() ).getGraveyard( 2 ).isVisible() );
+        }
+        /**
+         * Takes an appropriate action if the toggle game log button is clicked.
+         */
+        private void toggleGameLogHandler(){
+            ( (ChessPanel)getParent() ).getGameLog().setVisible(
+                !( (ChessPanel)getParent() ).getGameLog().isVisible() );
+            ( (ChessPanel)getParent() ).revalidate();
+        }
         
     }
     
-    
-    private void restartHandler(){
-        ( (ChessPanel)this.getParent() ).getGameEngine().reset();
-    }
-    /**
-     * Takes an appropriate action if the exit button is clicked.
-     * Uses Tony Allevato's code for exiting a GUI app without System.exit()
-     * calls.
-     */
-    private void exitHandler(){
-        JOptionPane.showMessageDialog( this.getParent(), "Thanks for leaving"
-            + ", quitter! >:(" );
-        Component possibleFrame = this;
-        while ( possibleFrame != null && !( possibleFrame instanceof JFrame ) ){
-            possibleFrame = possibleFrame.getParent();
-        }
-        JFrame frame = (JFrame)possibleFrame;
-        frame.setVisible( false );
-        frame.dispose();
-    }
-    /**
-     * Takes an appropriate action if the toggle graveyard button is clicked.
-     */
-    private void toggleGraveyardHandler(){
-        ( (ChessPanel)this.getParent() ).getGraveyard( 1 ).setVisible(
-            !( (ChessPanel)this.getParent() ).getGraveyard( 1 ).isVisible() );
-        ( (ChessPanel)this.getParent() ).getGraveyard( 2 ).setVisible(
-            !( (ChessPanel)this.getParent() ).getGraveyard( 2 ).isVisible() );
-    }
-    /**
-     * Takes an appropriate action if the toggle game log button is clicked.
-     */
-    private void toggleGameLogHandler(){
-        ( (ChessPanel)this.getParent() ).getGameLog().setVisible(
-            !( (ChessPanel)this.getParent() ).getGameLog().isVisible() );
-        ( (ChessPanel)this.getParent() ).revalidate();
-    }
 }
